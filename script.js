0a17070371e87ee45b487adbc5bacc6993b9d692
Leo Gharbaruk <ghabaruk@gmail.com>
2026-01-04 00:11:13 +0300

    messed everything up but got inpired again

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
-const btn = document.querySelector("button");
-const input = document.getElementById("textInput");
-const container = document.querySelector(".container");
-btn.addEventListener("click", localStorageTestFun);
-function localStorageTestFun() {
-    let inputValue = input.value;
-    localStorage.setItem("all", inputValue);
-    container.innerHTML = localStorage.getItem("all");
-}
-window.onload = () => {
-    container.innerHTML = localStorage.getItem("all");
-}
-
-
-
-// Ну типа я в шоке так легко
-// Но только надо еще научиться складывать несколько вещей в один ключ или хотя бы в несколько
+let word = "hello";
+const btnTrigger = document.querySelector(".add");
